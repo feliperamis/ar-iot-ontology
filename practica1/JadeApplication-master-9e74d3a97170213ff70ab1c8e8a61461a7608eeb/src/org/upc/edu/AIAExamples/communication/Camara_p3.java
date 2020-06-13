65fa6d791457e1833d18793a21fca04bb71f3d75
Alvaro <alvaro.macias@est.fib.upc.edu>
2020-06-13 20:15:45 +0200

    sensores y camara

---
+++ practica1/JadeApplication-master-9e74d3a97170213ff70ab1c8e8a61461a7608eeb/src/org/upc/edu/AIAExamples/communication/Camara_p3.java
@@ -0,0 +1,53 @@
+package org.upc.edu.AIAExamples.communication;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.AchieveREResponder;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Camara_p3 extends Agent {
+
+    private class CamaraResponder extends AchieveREResponder {
+        public CamaraResponder(Agent a, MessageTemplate mt) {
+            super(a, mt);
+        }
+
+        protected ACLMessage prepareResultNotification (ACLMessage request, ACLMessage response){
+            ACLMessage informDone = request.createReply();
+            informDone.setPerformative(ACLMessage.INFORM);
+            informDone.setContent("Possss toy viendo esto ns aver dime tu");
+            return informDone;
+        }
+
+    }
+
+    protected void setup() {
+
+        AID df = getDefaultDF();
+        DFAgentDescription camara = new DFAgentDescription();
+        ServiceDescription servicio  = new ServiceDescription();
+        servicio.setType( "Camara" );
+        servicio.setName( getLocalName() );
+        camara.addServices(servicio);
+        camara.setName(getAID());
+        try {
+            DFService.register(this, df, camara);
+        } catch (FIPAException ex) {
+            Logger.getLogger(Camara_p3.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        MessageTemplate mt = AchieveREResponder.createMessageTemplate((FIPANames.InteractionProtocol.FIPA_REQUEST));
+        this.addBehaviour(new Camara_p3.CamaraResponder(this, mt));
+    }
+}
---
+++ practica1/JadeApplication-master-9e74d3a97170213ff70ab1c8e8a61461a7608eeb/src/org/upc/edu/AIAExamples/communication/Termometro_p3.java
@@ -0,0 +1,66 @@
+package org.upc.edu.AIAExamples.communication;
+
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * @author Alvaro Macias
+ */
+public class Termometro_p3 extends Agent {
+
+    AID Enviroment;
+    public class SendMessageTickerBehaviour extends TickerBehaviour {
+        public SendMessageTickerBehaviour(Agent a, long period) {
+            super(a, period);
+        }
+
+        public void onTick() {
+            int t = (int)(Math.random()*40)-10;
+            ACLMessage message = new ACLMessage(ACLMessage.INFORM);
+            message.addReceiver(Enviroment);
+            message.setContent(Integer.toString(t));
+            send(message);
+        }
+    }
+
+    protected void setup() {
+
+        AID df = getDefaultDF();
+        DFAgentDescription register_template = new DFAgentDescription();
+        register_template.setName(getAID());
+        try {
+            DFService.register(this, df, register_template);
+        } catch (FIPAException ex) {
+            Logger.getLogger(Termometro_p3.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        DFAgentDescription busca = new DFAgentDescription();
+        ServiceDescription servicio  = new ServiceDescription();
+        servicio.setType( "Enviroment" );
+        busca.addServices(servicio);
+        Enviroment = new AID();
+        try {
+            DFAgentDescription[] result = DFService.search(this, busca);
+            if (result.length>0)
+                Enviroment = result[0].getName();
+        } catch (FIPAException ex) {
+            Logger.getLogger(Termometro_p3.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+        SendMessageTickerBehaviour Sensor = new SendMessageTickerBehaviour(this, 10000);
+        this.addBehaviour(Sensor);
+
+    }
+}
